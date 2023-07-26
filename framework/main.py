374b3c51982182c43bb785ffd7542c8529ecdbf5
Dmitry <hodas.work@gmail.com>
2023-07-26 19:33:46 +0300

    init commit

---
+++ framework/main.py
@@ -0,0 +1,25 @@
+
+class PageNotFound404:
+    def __call__(self, request):
+        return '404 WHAT', '404 PAGE Not Found'
+
+
+class Application:
+
+    def __init__(self, routes, fronts):
+        self.routes = routes
+        self.fronts = fronts
+
+    def __call__(self, environ, start_response):
+        path = environ['PATH_INFO']
+
+        if path in self.routes:
+            view = self.routes[path]
+        else:
+            view = PageNotFound404()
+        request = {}
+        for front in self.fronts:
+            front(request)
+        code, body = view(request)
+        start_response(code, [('Content-Type', 'text/html')])
+        return [body.encode('utf-8')]
